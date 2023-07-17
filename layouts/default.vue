b4336b36d68054a8b4ad02a6f6532a7955c87c79
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-07-17 09:57:27 +0700

    delete right drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,36 +29,12 @@
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>{{ title }}</v-toolbar-title>
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
   </v-app>
 </template>
 
@@ -86,7 +62,6 @@ export default {
         }
       ],
       right: true,
-      rightDrawer: false,
       title: 'Vuetify.js'
     }
   }
