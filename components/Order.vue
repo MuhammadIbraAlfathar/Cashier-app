48095feed0cdbd5844489109534221b0bbef6d70
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-07-26 13:03:59 +0700

    add title and price in order use getters

---
+++ components/Order.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <h2>Order</h2>
+      <v-list>
+        <v-list-item v-for="(item, index) in cartItems" :key="index">
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.title }}
+            </v-list-item-title>
+
+            <v-list-item-subtitle>
+              {{ item.price }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapState("carts", {
+      items: "items",
+    }),
+
+    ...mapGetters("carts", {
+      cartItems: "cartItem",
+    }),
+  },
+};
+</script>
